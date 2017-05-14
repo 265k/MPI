07c0ee5b84197773d65fa1d303f663383c245ed3
265k <265@protonmail.ch>
2017-05-14 21:49:56 +0300

    MPI added

--- integral/integral.c
+++ integral/integral.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include "mpi.h"
 
-/* A little convention that is used in this file:
+/* A little name convention that is used in this file:
    ll - lower limit
    ul - upper limit
    n - number of steps
    R - integration range
    f - target f
-   s - sum
-    */
+   ps - partial sum
+   ts - total sum*/
 
 double
 function (double x)
@@ -18,40 +19,66 @@ function (double x)
 }
 
 double
-rect (double (*f) (double), double ll, double ul, int n)
+rect (double (*f) (double), double ll, double ul, int n, int size, int rank)
 {
-  double s = 0, R, dx;
+  double ps = 0, ts = 0, R, dx;
   int i;
+
+  ul = (rank * 1.0 / size) * ul;
+  ll = (rank * 1.0 / size) * ll;
   R = ul - ll;
   dx = R / n;
+
   for (i = 0; i < n; i++)
-    s += (*f) (ll + dx * i) * dx;
-  return s;
+    ps += (*f) (ll + dx * i) * dx;
+
+  MPI_Reduce(&ps, &ts, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD); //0 represents the master process
+
+  return ts;
 }
 
 double
-trapez (double (*f) (double), double ll, double ul, int n)
+trapez (double (*f) (double), double ll, double ul, int n, int size, int rank)
 {
-  double s = 0, R, dx;
+  double ps = 0, ts = 0, R, dx;
   int i;
+  
+  ul = (rank * 1.0 / size) * ul;
+  ll = (rank * 1.0 / size) * ll;
   R = ul - ll;
   dx = R / n;
-  s += 0.5 * ((*f) (ll) + (*f) (ul)) * dx;
+
+  ps += 0.5 * ((*f) (ll) + (*f) (ul)) * dx;
   for (i = 1; i < n; i++)
-    s += (*f) (ll + dx * i) * dx;
-  return s;
+    ps += (*f) (ll + dx * i) * dx;
+
+  MPI_Reduce(&ps, &ts, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD); //0 represents the master process
+
+  return ts;
 }
 
 int
 main (int argc, char **argv)
 {
+  if (argc < 3)
+  {
+    printf("You must enter lower limit, upper limit and number of integration steps.\n");
+    return 1;
+  }
+  int size, rank;
   double (*f) (double);
   double ll = atof (argv[1]), ul = atof (argv[2]);
   int n = atoi (argv[3]);
   f = &function;
 
-  printf ("Rectangles: %f\n Trapeze: %f\n",
-	  rect (f, ll, ul, n), trapez (f, ll, ul, n));
+  MPI_Init(&argc, &argv);
 
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  printf ("Rectangles: %f\n Trapeze: %f\n",
+	  rect (f, ll, ul, n, size, rank), trapez (f, ll, ul, n, size, rank));
+
+  MPI_Finalize();
   return 0;
 }
