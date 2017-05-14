2b1fa7350017af2889c70cdaeb2fcdb29775e89f
265k <265@protonmail.ch>
2017-05-14 15:33:15 +0300

    Format fix

--- hello_world/hello_world.c
+++ hello_world/hello_world.c
@@ -7,11 +7,11 @@ main (int argc, char **argv)
   // Initialize the MPI environment
   MPI_Init (NULL, NULL);
 
-// Get the number of processes
+  // Get the number of processes
   int world_size;
   MPI_Comm_size (MPI_COMM_WORLD, &world_size);
 
-// Get the rank of the process
+  // Get the rank of the process
   int world_rank;
   MPI_Comm_rank (MPI_COMM_WORLD, &world_rank);
 
