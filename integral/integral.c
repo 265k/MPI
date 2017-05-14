79e47a1de85af9333e01c5913217852519fced27
265k <265@protonmail.ch>
2017-05-14 22:07:24 +0300

    Rank fixes

--- integral/integral.c
+++ integral/integral.c
@@ -15,7 +15,7 @@
 double
 function (double x)
 {
-  return exp(x);
+  return exp (x);
 }
 
 double
@@ -32,7 +32,7 @@ rect (double (*f) (double), double ll, double ul, int n, int size, int rank)
   for (i = 0; i < n; i++)
     ps += (*f) (ll + dx * i) * dx;
 
-  MPI_Reduce(&ps, &ts, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD); //0 represents the master process
+  MPI_Reduce (&ps, &ts, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);	//0 represents the master process
 
   return ts;
 }
@@ -42,7 +42,7 @@ trapez (double (*f) (double), double ll, double ul, int n, int size, int rank)
 {
   double ps = 0, ts = 0, R, dx;
   int i;
-  
+
   ul = (rank * 1.0 / size) * ul;
   ll = (rank * 1.0 / size) * ll;
   R = ul - ll;
@@ -52,7 +52,7 @@ trapez (double (*f) (double), double ll, double ul, int n, int size, int rank)
   for (i = 1; i < n; i++)
     ps += (*f) (ll + dx * i) * dx;
 
-  MPI_Reduce(&ps, &ts, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD); //0 represents the master process
+  MPI_Reduce (&ps, &ts, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);	//0 represents the master process
 
   return ts;
 }
@@ -61,24 +61,35 @@ int
 main (int argc, char **argv)
 {
   if (argc < 3)
-  {
-    printf("You must enter lower limit, upper limit and number of integration steps.\n");
-    return 1;
-  }
+    {
+      printf
+	("You must enter lower limit, upper limit and number of integration steps.\n");
+      return 1;
+    }
   int size, rank;
+  double ra, ta;
   double (*f) (double);
   double ll = atof (argv[1]), ul = atof (argv[2]);
   int n = atoi (argv[3]);
   f = &function;
 
-  MPI_Init(&argc, &argv);
+  MPI_Init (&argc, &argv);
 
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size (MPI_COMM_WORLD, &size);
+  MPI_Comm_rank (MPI_COMM_WORLD, &rank);
 
-  printf ("Rectangles: %f\n Trapeze: %f\n",
-	  rect (f, ll, ul, n, size, rank), trapez (f, ll, ul, n, size, rank));
+  if (!rank)
+    {
+      ra = rect (f, ll, ul, n, size, rank);
+      ta = trapez (f, ll, ul, n, size, rank);
+    }
 
-  MPI_Finalize();
+  if (rank == 0)
+    {
+      printf ("Rectangles: %f\n Trapeze: %f\n", ra, ta);
+    }
+    
+  MPI_Finalize ();
+
   return 0;
 }
