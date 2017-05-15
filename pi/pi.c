e003d5bf8ec4409e8b6549e3a22419170e0d1989
265k <265@protonmail.ch>
2017-05-15 12:24:30 +0300

    Fixed format

--- pi/pi.c
+++ pi/pi.c
@@ -1,26 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "mpi.h"
 
-int main(int argc, char** argv)
+
+int
+main (int argc, char **argv)
 {
-	int M = 0;
-	int N = 9999999;
-	long double x, y;
-	double pi;
-	time_t t;
+  int M = 0;
+  int N = 9999999;
+  long double x, y;
+  double pi;
+  time_t t;
+  int size, rank;
+  int MT;
+  int n;
 
-	srand((unsigned) time(&t));
+  srand ((unsigned) time (&t));
 
-	for (int i = 0; i < N; i++){
-		x = (2.0*rand()/RAND_MAX) - 1;
-		y = (2.0*rand()/RAND_MAX) - 1;
-		if (x*x + y*y <= 1.0) {
-			M++;
-		}
+  MPI_Init (&argc, &argv);
+
+  MPI_Comm_size (MPI_COMM_WORLD, &size);
+  MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+
+  n = N * (rank + 1) / size;
+  for (int i = N * rank / size; i < n; i++)
+    {
+      x = (2.0 * rand () / RAND_MAX) - 1;
+      y = (2.0 * rand () / RAND_MAX) - 1;
+      if (x * x + y * y <= 1.0)
+	{
+	  M++;
 	}
-
-	pi = 4.0*M/N;
-	printf("pi = %f\n", pi);
-	return 0;
+    }
+  MPI_Reduce (&M, &MT, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+  if (rank == 0)
+    {
+      pi = 4.0 * MT / N;
+      printf ("pi = %f\n", pi);
+    }
+  MPI_Finalize ();
+  return 0;
 }
